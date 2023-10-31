cc0aed520d2e2c98e59fc1d2f916fdb470660db7
Gabriel Pivato <pivatogabriel@gmail.com>
2023-10-30 21:37:20 -0300

    Freio e para trás

--- src/main.cpp
+++ src/main.cpp
@@ -15,8 +15,12 @@ void loop()
 {
   lineSensor.updateSensorsReadings();
 
-  if (lineSensor.irStates.rightLine && lineSensor.irStates.leftLine) // Fica Parado
-    motor.fullStop();
+  if (lineSensor.irStates.rightLine && lineSensor.irStates.leftLine)
+  { // Fica Parado
+    // motor.fullStop();
+    motor.turnWheel(Motor::RIGHT_WHEEL, Motor::BACKWARD, Motor::MID_SPEED);
+    motor.turnWheel(Motor::LEFT_WHEEL, Motor::BACKWARD, Motor::MID_SPEED);
+  }
   else if (!lineSensor.irStates.rightLine && !lineSensor.irStates.leftLine) // Vai p Frente
   {
     motor.turnWheel(Motor::RIGHT_WHEEL, Motor::FOWARD, Motor::SLOW_SPEED);
@@ -25,12 +29,12 @@ void loop()
   else if (lineSensor.irStates.rightLine) // Vai p Esquerda
   {
     motor.turnWheel(Motor::LEFT_WHEEL, Motor::FOWARD, Motor::MID_SPEED);
-    motor.turnWheel(Motor::RIGHT_WHEEL, Motor::BACKWARD, Motor::MID_SPEED);
+    motor.turnWheel(Motor::RIGHT_WHEEL, Motor::BACKWARD, Motor::SLOW_SPEED);
   }
   else if (lineSensor.irStates.leftLine) // Vai p Direita
   {
     motor.turnWheel(Motor::RIGHT_WHEEL, Motor::FOWARD, Motor::MID_SPEED);
-    motor.turnWheel(Motor::LEFT_WHEEL, Motor::BACKWARD, Motor::MID_SPEED);
+    motor.turnWheel(Motor::LEFT_WHEEL, Motor::BACKWARD, Motor::SLOW_SPEED);
   }
   else // Fica Parado
     motor.fullStop();
