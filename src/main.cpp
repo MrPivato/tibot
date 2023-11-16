6ff13654af3c1e1dc30b82373e89c0486c1f696d
Gabriel Pivato <pivatogabriel@gmail.com>
2023-11-15 22:15:01 -0300

    Alterações do dia 13/11

--- src/main.cpp
+++ src/main.cpp
@@ -18,23 +18,27 @@ void loop()
   if (lineSensor.irStates.rightLine && lineSensor.irStates.leftLine)
   { // Era Fica Parado
     // Agora anda p Tras
-    motor.turnWheel(Motor::RIGHT_WHEEL, Motor::BACKWARD, Motor::MID_SPEED);
-    motor.turnWheel(Motor::LEFT_WHEEL, Motor::BACKWARD, Motor::MID_SPEED);
+    motor.turnWheel(Motor::RIGHT_WHEEL, Motor::BACKWARD, Motor::HIGH_SPEED);
+    motor.turnWheel(Motor::LEFT_WHEEL, Motor::BACKWARD, Motor::HIGH_SPEED);
   }
   else if (!lineSensor.irStates.rightLine && !lineSensor.irStates.leftLine) // Vai p Frente
   {
-    motor.turnWheel(Motor::RIGHT_WHEEL, Motor::FOWARD, Motor::SLOW_SPEED);
-    motor.turnWheel(Motor::LEFT_WHEEL, Motor::FOWARD, Motor::SLOW_SPEED);
+    motor.turnWheel(Motor::RIGHT_WHEEL, Motor::FOWARD, Motor::MID_SPEED);
+    motor.turnWheel(Motor::LEFT_WHEEL, Motor::FOWARD, Motor::MID_SPEED);
   }
   else if (lineSensor.irStates.rightLine) // Vai p Esquerda
   {
-    motor.turnWheel(Motor::LEFT_WHEEL, Motor::FOWARD, Motor::MID_SPEED);
-    motor.turnWheel(Motor::RIGHT_WHEEL, Motor::BACKWARD, Motor::SLOW_SPEED);
+    // motor.turnWheel(Motor::LEFT_WHEEL, Motor::FOWARD, Motor::MID_SPEED);
+    // motor.turnWheel(Motor::RIGHT_WHEEL, Motor::BACKWARD, Motor::HIGH_SPEED);
+    motor.turnWheel(Motor::RIGHT_WHEEL, Motor::BACKWARD, Motor::HIGH_SPEED);
+    motor.stopWheel(Motor::LEFT_WHEEL);
   }
   else if (lineSensor.irStates.leftLine) // Vai p Direita
   {
-    motor.turnWheel(Motor::RIGHT_WHEEL, Motor::FOWARD, Motor::MID_SPEED);
-    motor.turnWheel(Motor::LEFT_WHEEL, Motor::BACKWARD, Motor::SLOW_SPEED);
+    // motor.turnWheel(Motor::RIGHT_WHEEL, Motor::FOWARD, Motor::MID_SPEED);
+    // motor.turnWheel(Motor::LEFT_WHEEL, Motor::BACKWARD, Motor::MID_SPEED);
+    motor.turnWheel(Motor::LEFT_WHEEL, Motor::BACKWARD, Motor::MID_SPEED);
+    motor.stopWheel(Motor::RIGHT_WHEEL);
   }
   else // Fica Parado
     motor.fullStop();
